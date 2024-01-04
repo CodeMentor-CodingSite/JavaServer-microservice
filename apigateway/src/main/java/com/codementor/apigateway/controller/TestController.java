952de76368ee8648e14d999d0fb8292c42018e9c
Hoonii2 <lkh66913@naver.com>
2024-01-04 16:00:47 +0900

    Add apigateway test code

---
+++ apigateway/src/main/java/com/codementor/apigateway/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.codementor.apigateway.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/api/apigateway/test")
+    public String test() {
+        return "This is apigateway service!";
+    }
+}
