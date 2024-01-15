a026cb57ad3e3e6727c11ee77a88587672eca19e
724thomas <724thomas@gmail.com>
2024-01-16 04:11:30 +0900

    Question서버 작업
    EvaluationDto 생성을 위한 Question정보를 담은 dto 생성
    execution서버에서 받는 요청 처리
    처리된 요청 execution서버로 다시 보내기
    Package(StudyPlan) 관련 엔터티 생성
    QuestionDifficulty enum 추가
    몇개의 엔터티 수정
    
    Resolves: #12

---
+++ question/src/main/java/com/codementor/question/entity/PlanMap.java
@@ -0,0 +1,26 @@
+package com.codementor.question.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Table(name = "package_map")
+public class PlanMap {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "package_map_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "package_id")
+    private Plan plan;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "question_id")
+    private Question question;
+}
