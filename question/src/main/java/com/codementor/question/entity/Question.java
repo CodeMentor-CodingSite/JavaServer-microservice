baa76c40aca8d3b7de441b697e7fcae7b4c50f7d
724thomas <724thomas@gmail.com>
2024-01-16 04:11:33 +0900

    Question서버 작업
    EvaluationDto 생성을 위한 Question정보를 담은 dto 생성
    execution서버에서 받는 요청 처리
    처리된 요청 execution서버로 다시 보내기
    Package(StudyPlan) 관련 엔터티 생성
    QuestionDifficulty enum 추가
    몇개의 엔터티 수정
    
    Resolves: #12

--- question/src/main/java/com/codementor/question/entity/Question.java
+++ question/src/main/java/com/codementor/question/entity/Question.java
@@ -1,5 +1,6 @@
 package com.codementor.question.entity;
 
+import com.codementor.question.enums.QuestionDifficulty;
 import lombok.*;
 
 import javax.persistence.*;
@@ -25,6 +26,22 @@ public class Question {
 
     @Column(name = "question_category", columnDefinition = "VARCHAR(40)")
     private String category;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = " questino_difficulty", columnDefinition = "VARCHAR(40)")
+    private QuestionDifficulty difficulty;
+
+    @Column(name = "question_likes", columnDefinition = "BIGINT(20)")
+    private Long likes = 0L;
+
+    @Column(name = "question_dislikes", columnDefinition = "BIGINT(20)")
+    private Long dislikes = 0L;
+
+    @Column(name = "question_attempted", columnDefinition = "BIGINT(20)")
+    private Long attempted = 0L;
+
+    @Column(name = "question_solved", columnDefinition = "BIGINT(20)")
+    private Long solved = 0L;
 
     @OneToMany(mappedBy = "question")
     private List<QuestionTestCase> questionTestCases;
