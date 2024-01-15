8d8f5bd9f0361f878d7c3d2f1c8a812b74adcc0a
724thomas <724thomas@gmail.com>
2024-01-16 04:11:17 +0900

    Question서버 작업
    EvaluationDto 생성을 위한 Question정보를 담은 dto 생성
    execution서버에서 받는 요청 처리
    처리된 요청 execution서버로 다시 보내기
    Package(StudyPlan) 관련 엔터티 생성
    QuestionDifficulty enum 추가
    몇개의 엔터티 수정
    
    Resolves: #12

---
+++ question/src/main/java/com/codementor/question/dto/evaluation/EvaluationDto.java
@@ -0,0 +1,35 @@
+package com.codementor.question.dto.evaluation;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class EvaluationDto {
+
+    private Long questionId;
+    private String questionTitle;
+    private String questionContent;
+    private String questionCategory;
+    private List<String> questionConstraints;
+
+
+    private List<EvalTestCaseDto> testCaseDtoList;
+    private String answerCheckContent;
+
+
+//    private Long userId;
+//    private String userLanguage;
+//    private String userCode;
+//
+//
+//    private List<String> testCaseResults;
+//    private String gptEvaluation;
+
+}
