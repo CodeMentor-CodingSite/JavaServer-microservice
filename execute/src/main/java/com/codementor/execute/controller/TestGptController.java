726eb8b70c3ad62c28d5947d872dd9c2071f0697
724thomas <724thomas@gmail.com>
2024-01-17 01:05:46 +0900

    Question서버 작업
    evaluationDto 관련 producer, consumer 수정
    엔터티 수정
    evaluation, question, execution 서버 로직 수정
    
    Resolves: #13

--- execute/src/main/java/com/codementor/execute/controller/TestGptController.java
+++ execute/src/main/java/com/codementor/execute/controller/TestGptController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-public class GptController {
+public class TestGptController {
 
     private final ChatService chatService;
 
