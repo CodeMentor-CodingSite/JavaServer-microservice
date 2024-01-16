cd3be6ea3ffd4309f7115180c2d6aba44eb1c656
724thomas <724thomas@gmail.com>
2024-01-17 01:05:23 +0900

    Question서버 작업
    evaluationDto 관련 producer, consumer 수정
    엔터티 수정
    evaluation, question, execution 서버 로직 수정
    
    Resolves: #13

--- execute/src/main/java/com/codementor/evaluate/dto/EvaluationDto.java
+++ execute/src/main/java/com/codementor/evaluate/dto/EvaluationDto.java
@@ -23,20 +23,18 @@ public class EvaluationDto {
      * questionContent: 질문 내용
      * questionCategory: 질문 카테고리
      * questionConstraints: 질문 제약사항
-     * testCaseDetailsDtoList: 질문 테스트 케이스(List<String> keys, List<String> values)로 이루어진 dto
      */
     private Long questionId;
     private String questionTitle;
     private String questionContent;
     private String questionCategory;
     private List<String> questionConstraints;
-    private List<QuestionTestCaseDto> questionTestCaseDtoList;
 
     /**
-     * codeExecutionConverterDto: 메서드 이름, 메서드 컨텐트, 리턴 타입을 담은 dto
+     * testCaseDtoList: 테스트 케이스와 해당 테스트케이스의 key, value, converter가 포함된 dto List를 포함하는 dto
      * answerCheckContent: input(특정 자료구조)을 유저코드에 매개변수로 넣고 나온 결과와, answer(특정 자료구조)가 같은지 확인하기 위한 코드
      */
-    private CodeExecutionConverterDto codeExecutionConverterDto;
+    private List<EvalQuestionTestCaseDto> testCaseDtoList;
     private String answerCheckContent;
 
     /**
