0abe99174922c07f7a368bd0969adb23d18e4b4f
724thomas <724thomas@gmail.com>
2024-01-21 23:05:56 +0900

    코드 제출 방식과 로직 수정
    Sse 적용
    Async 적용
    관련 dto 수정 및 통일
    
    #20

---
+++ execute/src/main/java/com/codementor/execute/repository/ExecuteUsercodeRepository.java
@@ -0,0 +1,11 @@
+package com.codementor.execute.repository;
+
+import com.codementor.execute.entity.ExecuteUsercode;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ExecuteUsercodeRepository extends JpaRepository<ExecuteUsercode, Long> {
+
+    List<Long> findAllByUserIdAAndIsCorrect(Long userId, Boolean isCorrect);
+}
