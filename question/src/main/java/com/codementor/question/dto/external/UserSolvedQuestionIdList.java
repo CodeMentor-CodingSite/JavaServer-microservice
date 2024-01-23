e695a0eb86028f3c1b4091a76a4378abdfcfcf80
724thomas <724thomas@gmail.com>
2024-01-24 04:55:06 +0900

    회원이 풀었던 문제들에 대한 정보(category, difficulty 포함)
    #29

---
+++ question/src/main/java/com/codementor/question/dto/external/UserSolvedQuestionIdList.java
@@ -0,0 +1,15 @@
+package com.codementor.question.dto.external;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserSolvedQuestionIdList {
+    private Long userId;
+    private List<Long> problemIdList;
+}
