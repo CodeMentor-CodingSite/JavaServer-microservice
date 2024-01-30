b5cf283c4dd711525c4553fbd5f8598bd2fb31a1
JeYeongR <wpdud2003@gmail.com>
2024-01-30 22:56:57 +0900

    Feat: AccessToken 블랙리스트 기능 구현

--- apigateway/src/main/java/com/codementor/apigateway/exception/TokenErrorEnum.java
+++ apigateway/src/main/java/com/codementor/apigateway/exception/TokenErrorEnum.java
@@ -10,7 +10,9 @@ public enum TokenErrorEnum {
     NOT_ADMIN_TOKEN(HttpStatus.FORBIDDEN, HttpStatus.FORBIDDEN.value(), "접근할 수 없습니다. Admin 사용자가 아닙니다"),
     NOT_USER_TOKEN(HttpStatus.FORBIDDEN, HttpStatus.FORBIDDEN.value(), "접근할 수 없습니다. User 사용자가 아닙니다"),
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, HttpStatus.UNAUTHORIZED.value(), "토큰이 필요합니다"),
-    EXPIRED_JWT(HttpStatus.UNAUTHORIZED, HttpStatus.UNAUTHORIZED.value(), "토큰이 만료되었습니다.");
+    EXPIRED_JWT(HttpStatus.UNAUTHORIZED, HttpStatus.UNAUTHORIZED.value(), "토큰이 만료되었습니다."),
+    LOGOUT_TOKEN(HttpStatus.UNAUTHORIZED, HttpStatus.UNAUTHORIZED.value(), "로그아웃된 토큰입니다.");
+
 
     private final HttpStatus status;
     private final int code;
