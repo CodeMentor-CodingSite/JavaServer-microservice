307ecb6a2f31d35c296a4755a76b4fb44327620a
724thomas <724thomas@gmail.com>
2024-01-30 22:15:04 +0900

    lets go

--- question/src/main/java/com/codementor/question/testApiConnection/TestController.java
+++ question/src/main/java/com/codementor/question/testApiConnection/TestController.java
@@ -1,7 +1,6 @@
 package com.codementor.question.testApiConnection;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +9,6 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class TestController {
     private final RestTemplate restTemplate;
-
-    @Value("${server.question.url}")
-    private String questionUrl;
 
     @Autowired
     public TestController(RestTemplate restTemplate) {
