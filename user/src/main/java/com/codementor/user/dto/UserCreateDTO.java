c66d84fca21ff944d17b30d4e3de1c09052084c8
JeYeongR <wpdud2003@gmail.com>
2024-02-02 21:06:32 +0900

    Feat: 회원가입 요청값 수정

--- user/src/main/java/com/codementor/user/dto/UserCreateDTO.java
+++ user/src/main/java/com/codementor/user/dto/UserCreateDTO.java
@@ -18,17 +18,11 @@ public class UserCreateDTO {
 
     private String nickname;
 
-    private String firstname;
-
-    private String lastname;
-
     public static User toEntity(UserCreateDTO userCreateDTO) {
         return User.builder()
                 .email(userCreateDTO.getEmail())
                 .password(userCreateDTO.getPassword())
                 .nickname(userCreateDTO.getNickname())
-                .firstname(userCreateDTO.getFirstname())
-                .lastname(userCreateDTO.getLastname())
                 .build();
     }
 }
