ed26a87e3dff67e4f4411a4d8c07af01b3692654
724thomas <724thomas@gmail.com>
2024-02-04 23:39:28 +0900

    toString 메서드 수정

---
+++ execute/src/main/java/com/codementor/dto/evaluation/EvalQuestionTestCaseDto.java
@@ -0,0 +1,29 @@
+package com.codementor.dto.evaluation;
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class EvalQuestionTestCaseDto {
+
+    private Long testCaseId;
+    private Boolean isExample;
+    private String explanation;
+    private List<EvalTestCaseDetailAndConverterDto> evalTestCaseDetailAndConverterDtos;
+    private String testCaseResult;
+
+    @Override
+    public String toString() {
+        return "EvalQuestionTestCaseDto{" + '\n' +
+                "testCaseId=" + testCaseId +'\n' +
+                ", isExample=" + isExample +'\n' +
+                ", explanation='" + explanation + '\'' +'\n' +
+                ", evalTestCaseDetailAndConverterDtos=" + evalTestCaseDetailAndConverterDtos.toString() +'\n' +
+                '}';
+    }
+}
