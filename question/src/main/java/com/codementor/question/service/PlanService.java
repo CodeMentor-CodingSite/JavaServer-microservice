05069b656e41dc19e32a342c2981fa3bfff3cb99
724thomas <724thomas@gmail.com>
2024-02-10 01:15:28 +0900

    feat: plan 입력
    #91

---
+++ question/src/main/java/com/codementor/question/service/PlanService.java
@@ -0,0 +1,37 @@
+package com.codementor.question.service;
+
+import com.codementor.question.dto.request.PlanInputRequest;
+import com.codementor.question.entity.Plan;
+import com.codementor.question.entity.PlanMap;
+import com.codementor.question.repository.PlanMapRepository;
+import com.codementor.question.repository.PlanRepository;
+import com.codementor.question.repository.QuestionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class PlanService {
+    private final PlanMapRepository planMapRepository;
+    private final PlanRepository planRepository;
+    private final QuestionRepository questionRepository;
+
+    public Long planInput(PlanInputRequest request) {
+        Plan plan = Plan.builder()
+                .planName(request.getName())
+                .planExplanation(request.getExplanation())
+                .build();
+        planRepository.save(plan);
+        List<PlanMap> planMaps = request.getQuestionIds().stream()
+                .map(questionId -> PlanMap.builder()
+                        .plan(plan)
+                        .question(questionRepository.findById(questionId).orElseThrow())
+                        .build())
+                .collect(Collectors.toList());
+        planMapRepository.saveAll(planMaps);
+        return plan.getId();
+    }
+}
