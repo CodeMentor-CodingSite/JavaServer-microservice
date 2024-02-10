68c9d99d1178ddc6b3d7d6281020e9603b543339
724thomas <724thomas@gmail.com>
2024-02-10 16:33:04 +0900

    feat: 사용자가 Plan 구독 및 구독해제
    #93

---
+++ user/src/main/java/com/codementor/user/repository/UserPlanRepository.java
@@ -0,0 +1,11 @@
+package com.codementor.user.repository;
+
+import com.codementor.user.entity.UserPlan;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserPlanRepository extends JpaRepository<UserPlan, Long>{
+
+    Optional<UserPlan> findByUserIdAndPlanId(Long userId, Long planId);
+}
