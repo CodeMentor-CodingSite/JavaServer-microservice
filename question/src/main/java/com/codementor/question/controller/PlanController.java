ade4c380886845aa0f92fa2aa5d263cece1c4f24
724thomas <724thomas@gmail.com>
2024-02-10 17:49:41 +0900

    refactor

--- question/src/main/java/com/codementor/question/controller/PlanController.java
+++ question/src/main/java/com/codementor/question/controller/PlanController.java
@@ -1,9 +1,9 @@
 package com.codementor.question.controller;
 
+import com.codementor.question.core.dto.ResponseDto;
 import com.codementor.question.dto.PlanDto;
 import com.codementor.question.service.PlanService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +17,7 @@ public class PlanController {
 
     private final PlanService planService;
     @GetMapping("/all")
-    public ResponseEntity<List<PlanDto>> getAllPlans() {
-        return ResponseEntity.ok(planService.getAllPlans());
+    public ResponseDto<List<PlanDto>> getAllPlans() {
+        return ResponseDto.ok(planService.getAllPlans());
     }
 }
