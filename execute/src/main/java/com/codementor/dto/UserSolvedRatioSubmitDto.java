56667c782c5fa3886f3ece91965cd8efa83e80f5
724thomas <724thomas@gmail.com>
2024-02-11 03:37:25 +0900

    refactor: moved converting logic to dtos and entities

--- execute/src/main/java/com/codementor/dto/UserSolvedRatioSubmitDto.java
+++ execute/src/main/java/com/codementor/dto/UserSolvedRatioSubmitDto.java
@@ -1,9 +1,13 @@
 package com.codementor.dto;
 
+import com.codementor.dto.external.UserSolvedQuestionIdList;
+import com.codementor.entity.ExecuteUsercode;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -12,4 +16,13 @@ import lombok.NoArgsConstructor;
 public class UserSolvedRatioSubmitDto {
     private Long userProblemSolvedCount;
     private Long userProblemSubmittedCount;
+
+    public static UserSolvedRatioSubmitDto from(List<ExecuteUsercode> executeUsercodeList) {
+        Long userSolved = executeUsercodeList.stream().filter(ExecuteUsercode::getIsCorrect).count();
+        Long userSubmitted = (long) executeUsercodeList.size();
+        return UserSolvedRatioSubmitDto.builder()
+                .userProblemSolvedCount(userSolved)
+                .userProblemSubmittedCount(userSubmitted)
+                .build();
+    }
 }
