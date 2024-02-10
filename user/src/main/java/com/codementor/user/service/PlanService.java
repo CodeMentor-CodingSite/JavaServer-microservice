da72ad367df0a3843b533428fa6f5fab0b3b7488
724thomas <724thomas@gmail.com>
2024-02-10 17:26:26 +0900

    feat: 사용자가 구독하고 있는 Plan 목록
    #94

--- user/src/main/java/com/codementor/user/service/PlanService.java
+++ user/src/main/java/com/codementor/user/service/PlanService.java
@@ -1,15 +1,26 @@
 package com.codementor.user.service;
 
+import com.codementor.user.core.util.RequestToServer;
+import com.codementor.user.dto.external.UserPlanDto;
 import com.codementor.user.entity.User;
 import com.codementor.user.entity.UserPlan;
 import com.codementor.user.repository.UserPlanRepository;
 import com.codementor.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class PlanService {
+
+    @Value("${server.question.url}")
+    private String questionoUrl;
+
+    private final RequestToServer requestToServer;
 
     private final UserPlanRepository userPlanRepository;
     private final UserRepository userRepository;
@@ -24,5 +35,14 @@ public class PlanService {
             userPlanRepository.save(UserPlan.subscribe(user, planId));
             return "Subscribed successfully";
         }
+    }
+
+    public List<UserPlanDto> getPlan(Long userId){
+        List<Long> userPlanIds = userPlanRepository.findAllByUserId(userId)
+                .stream()
+                .map(UserPlan::getPlanId)
+                .collect(Collectors.toList());
+        String reqUrl = questionoUrl + "/api/external/plan/get";
+        return requestToServer.postDataToServer(reqUrl, userPlanIds, List.class);
     }
 }
