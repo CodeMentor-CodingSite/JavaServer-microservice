2f8120b1fce7930ac8456b0218682f95bc069e23
724thomas <724thomas@gmail.com>
2024-02-11 03:36:59 +0900

    refactor: moved converting logic to dtos and entities

--- question/src/main/java/com/codementor/question/dto/external/QuestionDifficultyCounts.java
+++ question/src/main/java/com/codementor/question/dto/external/QuestionDifficultyCounts.java
@@ -1,5 +1,6 @@
 package com.codementor.question.dto.external;
 
+import com.codementor.question.entity.Question;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,4 +16,28 @@ public class QuestionDifficultyCounts {
     private Long mediumProblemsCount;
     private Long hardProblemsCount;
 
+    public static QuestionDifficultyCounts fromQuestionList(Iterable<Question> questionsList) {
+        Long easy = 0L;
+        Long medium = 0L;
+        Long hard = 0L;
+
+        for (Question question : questionsList) {
+            switch (question.getDifficulty()) {
+                case EASY:
+                    easy++;
+                    break;
+                case MEDIUM:
+                    medium++;
+                    break;
+                case HARD:
+                    hard++;
+                    break;
+            }
+        }
+        return QuestionDifficultyCounts.builder()
+                .easyProblemsCount(easy)
+                .mediumProblemsCount(medium)
+                .hardProblemsCount(hard)
+                .build();
+    }
 }
