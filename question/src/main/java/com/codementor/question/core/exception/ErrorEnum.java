2174f175b528963aa61d101e670a890c1c9afa37
724thomas <724thomas@gmail.com>
2024-02-10 17:32:31 +0900

    refactor: Error 통일

--- question/src/main/java/com/codementor/question/core/exception/ErrorEnum.java
+++ question/src/main/java/com/codementor/question/core/exception/ErrorEnum.java
@@ -13,6 +13,13 @@ public enum ErrorEnum {
     EXPIRED_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, 4011, "Expired access token"),
     INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, 4012, "Invalid refresh token"),
     EXPIRED_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, 4013, "Expired refresh token"),
+    EXIST_EMAIL(HttpStatus.CONFLICT, 4014, "해당 이메일은 사용중입니다"),
+    EXIST_NICKNAME(HttpStatus.CONFLICT, 4015, "해당 닉네임은 사용중입니다"),
+    INVALID_LOGIN_EMAIL(HttpStatus.BAD_REQUEST, 4016, "해당 이메일 계정이 존재하지 않습니다"),
+    INVALID_LOGIN_PASSWORD(HttpStatus.BAD_REQUEST, 4017, "비밀번호가 올바르지 않습니다"),
+    NOT_FOUND_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, 4018, "재로그인 해주세요."),
+    NOT_FOUND_USER_BY_USER_ID(HttpStatus.NOT_FOUND, 4019, "사용자 ID로 사용자를 찾을 수 없습니다"),
+    EXISTS_USER_LIKE(HttpStatus.CONFLICT, 4020, "이미 좋아요가 되어 있습니다."),
 
     // DB 관련 : 5000
     DATABASE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, 5000, "Database error"),
