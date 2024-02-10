a8485612e310bebcc142a90370a82dc268e832d6
724thomas <724thomas@gmail.com>
2024-02-11 03:36:47 +0900

    refactor: moved converting logic to dtos and entities

--- question/src/main/java/com/codementor/question/service/ExecutionHelperService.java
+++ question/src/main/java/com/codementor/question/service/ExecutionHelperService.java
@@ -21,8 +21,6 @@ import java.util.stream.Collectors;
 public class ExecutionHelperService {
     private final QuestionRepository questionRepository;
     private final LanguageRepository languageRepository;
-    private final QuestionLanguageRepository questionLanguageRepository;
-    private final QuestionTestCaseRepository questionTestCaseRepository;
     private final PlanRepository planRepository;
 
 
@@ -33,50 +31,22 @@ public class ExecutionHelperService {
         Question question = questionRepository.findById(questionId)
                 .orElseThrow(() -> new EntityNotFoundException("Question with ID " + questionId + " not found"));
 
-        String questionTitle = question.getTitle();
-        String questionContent = question.getContent();
-        String questionCategory = question.getCategory();
-        List<String> questionConstraints = new ArrayList<>();
-        for (QuestionConstraint qc : question.getQuestionConstraints()) {
-            questionConstraints.add(qc.getContent());
-        }
-        List<EvalTestCaseDto> testCaseDtoList = createTestCaseDtoList(question, language);
-        String answerCheckContent = getAnswerCheckContent(question, language);
+        List<String> questionConstraints = question.getQuestionConstraints().stream()
+                .map(QuestionConstraint::getContent)
+                .collect(Collectors.toList());
+
+        List<EvalTestCaseDto> testCaseDtoList = question.getQuestionTestCases().stream()
+                .map(testCase -> EvalTestCaseDto.from(testCase, createTestCaseDetailAndConverterDtoList(testCase, language)))
+                .collect(Collectors.toList());
+
+        String answerCheckContent = question.getQuestionLanguages().stream()
+                .filter(ql -> ql.getLanguage().equals(language))
+                .findFirst()
+                .map(QuestionLanguage::getCheckContent)
+                .orElse(null);
 
         //builder
-        return EvaluationDto.builder()
-                .questionId(questionId)
-                .questionTitle(questionTitle)
-                .questionContent(questionContent)
-                .questionCategory(questionCategory)
-                .questionConstraints(questionConstraints)
-                .testCaseDtoList(testCaseDtoList)
-                .answerCheckContent(answerCheckContent)
-                .build();
-    }
-
-    private String getAnswerCheckContent(Question question, Language language) {
-        Optional<QuestionLanguage> questionLanguage = questionLanguageRepository.findByQuestionAndLanguage(question, language);
-        return questionLanguage.map(QuestionLanguage::getCheckContent).orElse(null);
-    }
-
-    private List<EvalTestCaseDto> createTestCaseDtoList(Question question, Language language) {
-        List<EvalTestCaseDto> testCaseDtoList = new ArrayList<>();
-        List<QuestionTestCase> questionTestCaseList = questionTestCaseRepository.findByQuestion(question);
-
-        // QuestionTestCase들을 순회
-        for (QuestionTestCase testCase : questionTestCaseList) {
-            EvalTestCaseDto testCaseDto = EvalTestCaseDto.builder()
-                    .testCaseId(testCase.getId())
-                    .isExample(testCase.getIsExample())
-                    .explanation(testCase.getExplanation())
-                    // 각 QuestionTestCase에 연결된 QuestionTestCaseDetail들을 순회하며 해당 Language에 해당하는 Converter를 찾아 TestCaseDetailAndConverterDto를 생성
-                    .evalTestCaseDetailAndConverterDtos(createTestCaseDetailAndConverterDtoList(testCase, language))
-                    .testCaseResult("processing")
-                    .build();
-            testCaseDtoList.add(testCaseDto);
-        }
-        return testCaseDtoList;
+        return EvaluationDto.from(question, questionConstraints, testCaseDtoList, answerCheckContent);
     }
 
     private List<EvalTestCaseDetailAndConverterDto> createTestCaseDetailAndConverterDtoList(QuestionTestCase questionTestCase, Language language){
@@ -84,9 +54,7 @@ public class ExecutionHelperService {
 
         // QuestionTestCase와 연결된 QuestionTestCaseDetail들을 순회
         for (QuestionTestCaseDetail testCaseDetail : questionTestCase.getQuestionTestCaseDetails()) {
-            EvalTestCaseDetailAndConverterDto tcAndConv = new EvalTestCaseDetailAndConverterDto();
-            tcAndConv.setTestCaseKey(testCaseDetail.getKey());
-            tcAndConv.setTestCaseValue(testCaseDetail.getValue());
+            var tcAndConv = EvalTestCaseDetailAndConverterDto.from(testCaseDetail);
 
             // 각 QuestionTestCaseDetail에 연결된 ConverterMap들 중에서 해당 Language에 해당하는 Converter 찾기
             for (ConverterMap converterMap : testCaseDetail.getConverterMaps()) {
@@ -94,9 +62,7 @@ public class ExecutionHelperService {
 
                 // 해당 Language와 연결된 Converter만 처리
                 if (converter!=null && converter.getLanguage().equals(language)) {
-                    tcAndConv.setCodeExecConverterContent(converter.getContent());
-                    tcAndConv.setReturnType(converter.getReturnType());
-                    tcAndConv.setMethodName(converter.getMethodName());
+                    tcAndConv.updateWith(converter);
                 }
             }
             resultList.add(tcAndConv);
@@ -106,55 +72,11 @@ public class ExecutionHelperService {
 
     public QuestionDifficultyCounts getAllQuestionsDifficultyCounts(){
         List<Question> questionsList = questionRepository.findAll();
-        Long easy = 0L;
-        Long medium = 0L;
-        Long hard = 0L;
-
-        for (Question question : questionsList) {
-            switch (question.getDifficulty()) {
-                case EASY:
-                    easy++;
-                    break;
-                case MEDIUM:
-                    medium++;
-                    break;
-                case HARD:
-                    hard++;
-                    break;
-            }
-        }
-        return QuestionDifficultyCounts.builder()
-                .easyProblemsCount(easy)
-                .mediumProblemsCount(medium)
-                .hardProblemsCount(hard)
-                .build();
+        return QuestionDifficultyCounts.fromQuestionList(questionsList);
     }
 
     public UserSolvedRatioTotalDto getUserSolvedRatioSubmitDto(UserSolvedRatioTotalDto req){
-        Long easyProblemSolvedCount = 0L;
-        Long mediumProblemSolvedCount = 0L;
-        Long hardProblemSolvedCount = 0L;
-
-        // make a hashset for unique user problem list
-        HashSet<Long> questionSolvedList = new HashSet<>();
-        questionSolvedList.addAll(req.getQuestionIdList());
-        List<Question> questionsList = questionRepository.findAll();
-        for (Question question : questionsList) {
-            if (questionSolvedList.contains(question.getId())) {
-                switch (question.getDifficulty()) {
-                    case EASY:
-                        easyProblemSolvedCount++;
-                        break;
-                    case MEDIUM:
-                        mediumProblemSolvedCount++;
-                        break;
-                    case HARD:
-                        hardProblemSolvedCount++;
-                        break;
-                }
-            }
-        }
-        return req.addProblemSolvedCounts(easyProblemSolvedCount, mediumProblemSolvedCount, hardProblemSolvedCount);
+        return req.updatedWith(req, questionRepository.findAll());
     }
 
     public UserSolvedCategoryDtoList getUserSolvedCategoryQuestionList(UserSolvedQuestionIdList req){
@@ -173,13 +95,7 @@ public class ExecutionHelperService {
         for (var req : request) {
             Optional<Question> question = questionRepository.findByIdAndDifficulty(req.getQuestionId(), QuestionDifficulty.valueOf(req.getDifficulty()));
             if (question.isPresent()){
-                newResponse.add(UserSolvedQuestionIdAndTitleAndTimeResponse.builder()
-                                .usercodeId(req.getUsercodeId())
-                                .questionId(question.get().getId())
-                                .questionTitle(question.get().getTitle())
-                                .difficulty(req.getDifficulty())
-                                .timeStamp(req.getTimeStamp())
-                                .build());
+                newResponse.add(UserSolvedQuestionIdAndTitleAndTimeResponse.of(req, question));
             }
         }
         return newResponse;
