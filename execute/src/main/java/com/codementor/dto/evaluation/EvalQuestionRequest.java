ea0b74a1315cd111e677f1b31968825febba06de
724thomas <724thomas@gmail.com>
2024-02-11 03:36:23 +0900

    refactor: moved converting logic to dtos and entities

--- execute/src/main/java/com/codementor/dto/evaluation/EvalQuestionRequest.java
+++ execute/src/main/java/com/codementor/dto/evaluation/EvalQuestionRequest.java
@@ -1,5 +1,6 @@
 package com.codementor.dto.evaluation;
 
+import com.codementor.dto.request.UserCodeExecutionRequest;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,4 +13,11 @@ import lombok.NoArgsConstructor;
 public class EvalQuestionRequest {
     private Long questionId;
     private String userLanguage;
+
+    public static EvalQuestionRequest from(UserCodeExecutionRequest userCodeExecutionRequest) {
+        return EvalQuestionRequest.builder()
+                .questionId(userCodeExecutionRequest.getQuestionId())
+                .userLanguage(userCodeExecutionRequest.getUserLanguage())
+                .build();
+    }
 }
