3af559ab63c298f9345e1d72737b8d9d8c223d86
724thomas <724thomas@gmail.com>
2024-02-11 03:37:20 +0900

    refactor: moved converting logic to dtos and entities

--- execute/src/main/java/com/codementor/dto/external/UserQuestionsStatus.java
+++ execute/src/main/java/com/codementor/dto/external/UserQuestionsStatus.java
@@ -1,11 +1,15 @@
 package com.codementor.dto.external;
 
+import com.codementor.entity.ExecuteUsercode;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Data
 @NoArgsConstructor
@@ -14,4 +18,20 @@ import java.util.List;
 public class UserQuestionsStatus {
     private List<Long> attmptedQuestions;
     private List<Long> solvedQuestions;
+
+    public static UserQuestionsStatus from(List<ExecuteUsercode> executeUsercodeList) {
+        Set<Long> attemptedQuestionIds = new HashSet<>();
+        Set<Long> solvedQuestionIds = new HashSet<>();
+        for (ExecuteUsercode executeUsercode : executeUsercodeList) {
+            if (executeUsercode.getIsCorrect()) {
+                solvedQuestionIds.add(executeUsercode.getQuestionId());
+            } else {
+                attemptedQuestionIds.add(executeUsercode.getQuestionId());
+            }
+        }
+        return UserQuestionsStatus.builder()
+                .attmptedQuestions(new ArrayList<>(attemptedQuestionIds))
+                .solvedQuestions(new ArrayList<>(solvedQuestionIds))
+                .build();
+    }
 }
