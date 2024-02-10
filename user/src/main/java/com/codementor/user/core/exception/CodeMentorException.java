393852bb76a787fdf6037e7cb7ca0c31c852f54d
724thomas <724thomas@gmail.com>
2024-02-10 17:26:01 +0900

    feat: 사용자가 구독하고 있는 Plan 목록
    #94

---
+++ user/src/main/java/com/codementor/user/core/exception/CodeMentorException.java
@@ -0,0 +1,34 @@
+package com.codementor.user.core.exception;
+
+import com.codementor.user.dto.dto.ErrorDto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.ResponseEntity;
+
+@Getter
+@AllArgsConstructor
+public class CodeMentorException extends RuntimeException {
+
+    private final ErrorEnum errorEnum;
+
+    public ResponseEntity<ErrorDto> getResponse() {
+        return new ResponseEntity<>(
+                errorEnum.getErrorDto(),
+                errorEnum.getHttpStatus()
+        );
+    }
+
+    public ResponseEntity<ErrorDto> getResponse(String message) {
+        return new ResponseEntity<>(
+                errorEnum.getErrorDto(message),
+                errorEnum.getHttpStatus()
+        );
+    }
+
+    public ResponseEntity<ErrorDto> getResponse(ErrorEnum errorEnum) {
+        return new ResponseEntity<>(
+                errorEnum.getErrorDto(),
+                errorEnum.getHttpStatus()
+        );
+    }
+}
