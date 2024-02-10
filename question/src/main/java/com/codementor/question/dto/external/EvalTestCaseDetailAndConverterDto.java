e5746f4676a21fbb6cb99f49aeb5820baf6aa75d
724thomas <724thomas@gmail.com>
2024-02-11 03:36:26 +0900

    refactor: moved converting logic to dtos and entities

--- question/src/main/java/com/codementor/question/dto/external/EvalTestCaseDetailAndConverterDto.java
+++ question/src/main/java/com/codementor/question/dto/external/EvalTestCaseDetailAndConverterDto.java
@@ -1,5 +1,7 @@
 package com.codementor.question.dto.external;
 
+import com.codementor.question.entity.CodeExecConverter;
+import com.codementor.question.entity.QuestionTestCaseDetail;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,4 +20,17 @@ public class EvalTestCaseDetailAndConverterDto {
     // QuestionTestCaseDetail 필드
     private String testCaseKey;
     private String testCaseValue;
+
+    public void updateWith(CodeExecConverter converter){
+        this.codeExecConverterContent= converter.getContent();
+        this.returnType = converter.getReturnType();
+        this.methodName = converter.getMethodName();
+    }
+
+    public static EvalTestCaseDetailAndConverterDto from(QuestionTestCaseDetail detail){
+        return EvalTestCaseDetailAndConverterDto.builder()
+                .testCaseKey(detail.getKey())
+                .testCaseValue(detail.getValue())
+                .build();
+    }
 }
